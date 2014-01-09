ad8d8d4525c69f4af53ebfebdfa78a00a738a3e5
SDT1 <eyebeam79@naver.com>
2014-01-09 10:31:47 +0900

    Initial Commit

---
+++ ModelDataTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ModelDataTest
+//
+//  Created by SDT1 on 2014. 1. 9..
+//  Copyright (c) 2014년 SDT1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
